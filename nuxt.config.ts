00e0533b6d8043574183c7a1afae372bcde27f78
Peeratchai Kleebbua <peeratchai.kleebbua@gmail.com>
2025-10-28 01:18:32 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: "New Tracking Platform",
-    },
-  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/tailwind.css"],
